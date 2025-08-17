10d9584b6389387f2a5151349e0af5b72777b4c9
Shreshth Sharma <shraeshth@Shreshths-MacBook-Air.local>
2025-08-17 23:12:57 +0530

    fixed expanding bug

--- src/page/Background.jsx
+++ src/page/Background.jsx
@@ -10,28 +10,28 @@ function Background() {
     gsap
       .timeline({ repeat: -1, yoyo: true })
       .to(bgRef.current, {
-        scale: 1.3,
-        x: 500, // move right
+        scale: 1.5,
+        x: 100, // move right
         y: -100, // move up
-        duration: 15,
+        duration: 10,
         ease: "power1.inOut",
       })
       .to(bgRef.current, {
-        scale: 1.1,
+        scale: 1,
         x: -100, // move left
-        y: 200, // move down
-        duration: 15,
+        y: 100, // move down
+        duration: 10,
         ease: "power1.inOut",
       });
   }, []);
 
   return (
-    <div className="absolute inset-0 z-[-10]">
+    <div className="absolute inset-0 z-[-10] overflow-hidden">
       <img
         ref={bgRef}
         src={bgImage}
         alt="Background"
-        className="w-[1000px] h-auto object-cover"
+        className="w-[80%] h-auto object-cover"
       />
     </div>
   );
